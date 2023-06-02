68b0a4d3b80dd79deefa32ce2c32a608d76f9df0
Eunyeong Jang <jangeunyeong081@gmail.com>
2023-06-02 10:33:35 +0900

    style : eslint,prettier 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,9 +9,9 @@ export default defineConfig({
   },
   resolve: {
     alias: [
-      { find: "@assets", replacement: "/src/assets" },
-      { find: "@components", replacement: "/src/components" },
-      { find: "@", replacement: "/src" },
+      { find: '@assets', replacement: '/src/assets' },
+      { find: '@components', replacement: '/src/components' },
+      { find: '@', replacement: '/src' },
     ],
   },
 });
