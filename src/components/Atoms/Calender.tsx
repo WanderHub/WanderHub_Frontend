cc7147fc2870943db767fff3572cdeb533a62528
jg <jgkang9402@gmail.com>
2023-06-04 21:08:34 +0900

    feat : react-calendar 컴포넌트생성

---
+++ src/components/Atoms/Calender.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+const Calender = () => {
+  return (
+    <div className="w-3/6">
+      <Calendar />
+    </div>
+  );
+};
+
+export default Calender;
