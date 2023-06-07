95e4661135d043dba35b589793ce45a6004d876e
jg <jgkang9402@gmail.com>
2023-06-07 15:59:10 +0900

    feat : 동행 글작성페이지 생성, useRouter훅생성

--- src/components/Molecules/Title.tsx
+++ src/components/Molecules/Title.tsx
@@ -5,13 +5,14 @@ interface TitleProps {
   title: string;
   isWithBtn: boolean;
   btnTxt?: string;
+  clickEvent?: () => void;
 }
 
-const Title = ({ title, isWithBtn, btnTxt }: TitleProps) => {
+const Title = ({ title, isWithBtn, btnTxt, clickEvent }: TitleProps) => {
   return (
     <div className="border-2 shadow-md inset h-16 rounded-full flex items-center justify-between px-4">
       <h2 className="font-bold text-3xl flex-grow text-center ">{title}</h2>
-      {isWithBtn && <Button clickEvent={() => console.log('click')}>{btnTxt ?? ''}</Button>}
+      {isWithBtn && clickEvent && <Button clickEvent={clickEvent}>{btnTxt ?? ''}</Button>}
     </div>
   );
 };
