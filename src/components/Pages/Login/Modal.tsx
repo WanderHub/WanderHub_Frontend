d3cc404932f2536d3828dfc769ae8f136fbd5afc
Eunyeong Jang <jangeunyeong081@gmail.com>
2023-06-13 16:47:39 +0900

    style : Atoms component 재사용 -> html 태그로 수정

--- src/components/Pages/Login/Modal.tsx
+++ src/components/Pages/Login/Modal.tsx
@@ -1,9 +1,7 @@
 import React, { SetStateAction } from 'react';
-import Button from '@components/Atoms/Button';
 import Logo from '@assets/white_bg_logo.png';
 import GoogleSymbol from '@assets/google_symbol.png';
 import KaKaoSymbol from '@assets/kakao_symbol.png';
-import Image from '@components/Atoms/Image';
 import XMarkIcon from '@assets/cross.png';
 
 export interface ModalProps {
@@ -24,10 +22,10 @@ const Modal = ({ setOpenModal }: ModalProps) => {
                   className="absolute right-8 top-6"
                   onClick={() => setOpenModal(false)}
                 >
-                  <Image src={XMarkIcon} alt="close button" classNameProps="h-4 w-4" />
+                  <img src={XMarkIcon} alt="close button" className="h-4 w-4" />
                 </button>
                 <div className=" sm:w-full sm:max-w-sm">
-                  <Image classNameProps="w-52 h-19 mb-5" src={Logo} alt="WanderHub Logo" />
+                  <img className="w-52 h-19 mb-5" src={Logo} alt="WanderHub Logo" />
                   <h2 className="mb-3 text-3xl font-bold leading-9 tracking-tight text-gray-900">
                     소셜 로그인
                   </h2>
@@ -35,28 +33,26 @@ const Modal = ({ setOpenModal }: ModalProps) => {
                 <p className="mb-large text-base text-gray-900">
                   동행을 찾아 함께 여행해 보세요 :)
                 </p>
-                <Button
-                  classNameProps="flex justify-center w-full h-14 rounded-lg px-20 py-3 bg-primary hover:bg-neutral-400"
+                <button
+                  className="flex justify-center w-full h-14 rounded-lg px-20 py-3 bg-primary hover:bg-neutral-400"
                   type="button"
-                  clickEvent={() => console.log('click')}
                 >
-                  <Image
+                  <img
                     src={GoogleSymbol}
                     width={20}
                     height={20}
                     alt="GoogleSymbol"
-                    classNameProps="pt-1.5 hover:bg-black"
+                    className="pt-1.5 hover:bg-black"
                   />
                   <span className="px-5 font-semibold mt-1">구글 계정 로그인</span>
-                </Button>
-                <Button
-                  classNameProps="mt-3 flex justify-center w-full h-14 rounded-lg py-3 bg-kakao hover:bg-neutral-400"
+                </button>
+                <button
+                  className="mt-3 flex justify-center w-full h-14 rounded-lg py-3 bg-kakao hover:bg-neutral-400"
                   type="button"
-                  clickEvent={() => console.log('click')}
                 >
-                  <Image src={KaKaoSymbol} alt="GoogleSymbol" classNameProps="h-full" />
+                  <img src={KaKaoSymbol} alt="GoogleSymbol" className="h-full" />
                   <span className="px-3 font-semibold mt-1">카카오 계정 로그인</span>
-                </Button>
+                </button>
               </div>
             </div>
           </div>
