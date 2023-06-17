afee5606f93f0066e4c009c323f483458425d64d
jg <jgkang9402@gmail.com>
2023-06-17 13:04:58 +0900

    fix : 테스트더미데이터 키값수정및 카드 프롭스 수정

--- src/components/accompany/Card.tsx
+++ src/components/accompany/Card.tsx
@@ -1,25 +1,33 @@
+import useRouter from '@/hooks/useRouter';
 import React from 'react';
 
 export type CardProps = {
-  title: string;
-  curPeople: number;
-  maxPeople: number;
-  location: string;
-  imgPath: string;
+  id: number;
+  memberId: number;
+  writerName: string;
+  accompanyLocal: string;
+  accompanyDate: string;
+  maxNum: number;
+  accompanyTitle: string;
+  accompanyContent: string;
+  openStatus: boolean;
+  createdAt: null; // 현재 어느타입으로 오는지모름 null | string일듯
+  modifiedAt: null; // 현재 어느타입으로 오는지모름 null | string일듯
 };
 
-const Card = ({ title, curPeople, maxPeople, location, imgPath }: CardProps) => {
+const Card = (props: CardProps) => {
+  const { id, accompanyLocal, maxNum, accompanyTitle, accompanyContent } = props;
+  const { goTo } = useRouter();
   return (
-    <div className="border border-gray-300 rounded-2xl overflow-hidden cursor-pointer">
-      <div className="h-[65%]">
-        <img src={imgPath} alt={title} className="w-full h-full" />
-      </div>
-      <div className="p-4">
-        <p>{location}</p>
-        <h3 className="text-lg font-bold mb-2">{title}</h3>
-        <p className="bt border-gray-300 text-end">
-          현재인원 :&nbsp;{curPeople}/{maxPeople}
-        </p>
+    <div
+      onClick={() => goTo(`/accompany/${id}`)}
+      className="border border-gray-300 p-[.5rem] rounded-2xl overflow-hidden cursor-pointer"
+    >
+      <div className="p-4 bg-gray-200 rounded-2xl">
+        <div className="text-end">{accompanyLocal}</div>
+        <h3 className="text-lg font-bold mb-2">{accompanyTitle}</h3>
+        <p>{accompanyContent}</p>
+        <p className="bt border-gray-300 text-end">현재인원 :&nbsp;{maxNum}</p>
       </div>
     </div>
   );
