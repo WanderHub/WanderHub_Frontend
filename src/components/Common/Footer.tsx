e00793390902d14fcfec1cc7a279e5e5c0f9e3e5
J9 <93317914+jgkang9402@users.noreply.github.com>
2023-06-29 14:12:54 +0900

    Feat : mypage 유아이생성, 헤더&푸터&컨테이너 높이값 수정 (#19)
    * feat : 마이페이지 기초레이아웃생성
    
    * design : 전체레이아웃 수정
    
    * fix : 지도,지역관련데이터 수정
    
    * feat : 마이페이지 나의글관련 ui생성(테이블,페이지네이션,상단탭)
    
    * design : 마이페이지 개인정보 유아이수정

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-primary text-white py-4  w-full border-t border-gray-300">
+    <footer className="bg-primary text-white py-4  w-full border-t border-gray-300 h-[10vh]">
       <div className="container mx-auto text-center">
         <p>&copy; 2023 WanderHub. All rights reserved.</p>
-        <p>Produce by 강정구, 김성수, 김하은, 장은영</p>
+        <p>Produce by 강정구, 김하은, 장은영</p>
       </div>
     </footer>
   );
