3127960493c6fc8ff9fe17d55f2e5fda7e8d86af
jg <jgkang9402@gmail.com>
2023-07-09 01:34:09 +0900

    feat : travelapi 인스턴스생성, useGetTravelList훅생성

---
+++ src/hooks/queryHooks/useGetTravelList.tsx
@@ -0,0 +1,29 @@
+import { TravelApi } from '@/api/TravelApi';
+import { useQuery } from 'react-query';
+
+const useGetTravelList = (code: string) => {
+  const { data, isLoading, error } = useQuery(
+    ['travelList', code],
+    async () => {
+      try {
+        const response = await TravelApi.get(
+          `/areaBasedList1?serviceKey=${
+            import.meta.env.VITE_TRAVEL_API_KEY
+          }&MobileOS=WIN&MobileApp=AppTest&_type=json&numOfRows=20&${code.replace('?', '')}`,
+        );
+        return response.data.response.body;
+      } catch (error) {
+        console.log('@@@@', error);
+        throw error;
+      }
+    },
+    {
+      staleTime: 50000,
+      // staleTime: Infinity,
+    },
+  );
+
+  return { data, isLoading, error };
+};
+
+export default useGetTravelList;
