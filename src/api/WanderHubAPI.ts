590ba4a5853c1762a633a8e7924238a70c3fe43a
eunyeong <123078739+jjangeunyeong@users.noreply.github.com>
2023-07-19 19:58:39 +0900

    fix : cors error 해결 (#26)
    * feat : CommunityDetail page UI추가
    
    * feat : 로그인 api 연동
    
    * feat : proxy 설정
    
    * fix : cors error 해결

--- src/api/WanderHubAPI.ts
+++ src/api/WanderHubAPI.ts
@@ -3,11 +3,26 @@ import axios, { AxiosRequestConfig } from 'axios';
 import { WANDERHUB } from '@/api/BASEURL';
 
 const instance = axios.create({
-  baseURL: WANDERHUB,
+  baseURL: '/v1',
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true,
+});
+
+instance.interceptors.request.use(request => {
+  const accessToken = localStorage.getItem('accessToken');
+  if (accessToken !== null) {
+    request.headers.authorization = `Bearer ${accessToken}`;
+  }
+  return request;
+});
+
+instance.interceptors.response.use(response => {
+  const accessToken = localStorage.getItem('accessToken');
+  response.headers = {
+    authorization: `Bearer ${accessToken}`,
+  };
+  return response;
 });
 
 const WanderHubAPI = {
