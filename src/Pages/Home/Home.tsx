c8df5484423549ea1af9cf859a97d8258b41423a
Eunyeong Jang <jangeunyeong081@gmail.com>
2023-07-20 14:56:04 +0900

    fix : 절대경로 수정

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import Footer from '@components/Common/Footer';
 import Header from '@components/Common/Header';
-import Modal from '@/pages/Login/Modal';
-import FestivalCarousel from '@/pages/Home/FestivalCarousel';
-import AccompanyCarousel from '@/pages/Home/AccompanyCarousel';
+import Modal from '@pages/Login/Modal';
+import FestivalCarousel from '@pages/Home/FestivalCarousel';
+import AccompanyCarousel from '@pages/Home/AccompanyCarousel';
 
 const Home = () => {
   const [openModal, setOpenModal] = useState(false);
